231503d4b06566b0ada28423e00c7e36fac19ac5
Sk3ll <sk3llyar@gmail.com>
2020-11-04 12:13:27 +0200

    fix problem

--- client/app.js
+++ client/app.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Rental from "./components/rental";
 import New from "./components/new";
-import Index from "./components/available";
+import Index from "./components/index";
 
 
 const App = () => (
