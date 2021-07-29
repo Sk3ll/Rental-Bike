b2a5b02ff517d4d0b5e72b7f49168b9fc5a8243a
Yaroslav Malikhin <yaroslav.malykhin@aimprosoft.com>
2021-07-29 19:30:20 +0300

    Delete unused files, changed tests

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BikesModule } from './bikes/bikes.module';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost/nest'), BikesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- server/src/bikes/bikes.controller.spec.ts
+++ server/src/bikes/bikes.controller.spec.ts
@@ -1,20 +1,35 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test, TestingModule } from '@nestjs/2.00testing';
+import { MongooseModule } from '@nestjs/mongoose';
 import { BikesController } from './bikes.controller';
 import { BikesService } from './bikes.service';
+import { Bike, BikeSchema } from './entities/bike.entity';
 
 describe('BikesController', () => {
-  let controller: BikesController;
+  let bikesController: BikesController;
+  let bikesService: BikesService;
+  const mockBike: Bike = {
+    _id: 'faf182e5-0226-4bbb-9035-fae2b81d7826',
+    name: 'string',
+    typeBike: 'string',
+    price: 100,
+    isRental: false,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [MongooseModule.forFeature([{ name: Bike.name, schema: BikeSchema }])],
       controllers: [BikesController],
       providers: [BikesService],
     }).compile();
 
-    controller = module.get<BikesController>(BikesController);
+    bikesController = module.get<BikesController>(BikesController);
+    bikesService = module.get<BikesService>(BikesService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('find all', async () => {
+    const result: Bike[] = [mockBike];
+    jest.spyOn(bikesService, 'findAll').mockImplementation(() => Promise.resolve(result));
+
+    expect(await bikesController.findRentals()).toBe(result);
   });
 });
